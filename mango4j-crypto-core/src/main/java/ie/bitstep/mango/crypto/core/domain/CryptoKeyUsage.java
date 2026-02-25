6bfa3f25d78a05d294a9a2a4500f5fac9ff87735
e048222 <madalin.druta@mastercard.com>
2026-02-25 17:47:23 +0000

    Trying to remove things

--- mango4j-crypto-core/src/main/java/ie/bitstep/mango/crypto/core/domain/CryptoKeyUsage.java
+++
@@ -1,9 +0,0 @@
-package ie.bitstep.mango.crypto.core.domain;
-
-/**
- * Represents the type of cryptographic operations that the associated {@link CryptoKey} will perform.
- */
-public enum CryptoKeyUsage {
-	ENCRYPTION,
-	HMAC
-}
